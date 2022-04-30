1b7dca0c8dea77eb667a20beab1c363e547411ca
Guto Xavier <gutoffline@gmail.com>
2022-04-30 11:35:34 -0300

    Exercício 07

--- exercicios/01-estrutura-sequencial/ex007.py
+++ exercicios/01-estrutura-sequencial/ex007.py
@@ -1,1 +1,5 @@
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+lado = float(input("Informe o valor de um lado: "))
+area = lado * lado
+dobro = area * 2
+print("O dobro da área é:", dobro)
