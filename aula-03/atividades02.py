ac3e413a3b9fb6fc4dacecbfd927357b8fb08e59
Guto Xavier <gutoffline@gmail.com>
2022-04-30 13:54:14 -0300

    Operadores relacionais

---
+++ aula-03/atividades02.py
@@ -0,0 +1,18 @@
+# Operadores Relacionais
+print(10 > 3)
+
+# > --> maior
+# < --> menor
+# >= --> maior ou igual
+# <= --> menor ou igual
+# != --> diferente
+# == --> igual
+# IMPORTANTE
+# == é o operador de comparação
+# = é o operador de atribuição
+
+print( 5+2 > 10)
+# ______ > ______ ---> True / False
+print("10 < 10", 10<10)
+print("10 == 10", 10==10)
+print("10 <= 10", 10<=10)
