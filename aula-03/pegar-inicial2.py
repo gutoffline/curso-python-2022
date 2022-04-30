c1f30b78b56c64ad756643b88926cfc98f966e3e
Guto Xavier <gutoffline@gmail.com>
2022-04-30 13:44:08 -0300

    operações com string

---
+++ aula-03/pegar-inicial2.py
@@ -0,0 +1,7 @@
+clientes = "João Paulo,Luiz,Maria"
+lista_clientes = clientes.split(',')
+print(lista_clientes)
+print(len(lista_clientes))
+
+print(lista_clientes[0][0])
+print(lista_clientes[1][0])
