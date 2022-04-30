afb2d6a2c4323554807bb3d09fe6f77d56fe31ca
Guto Xavier <gutoffline@gmail.com>
2022-04-30 11:21:37 -0300

    Exercício do raio

---
+++ exercicios/01-estrutura-sequencial/ex006.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+
+import math
+
+raio = float(input('Informe o raio do círculo: '))
+area = math.pi * raio ** 2
+print('A área do círculo é:', area)
