5c36550c5fa85ec475cd15a32f0b29d56e0eb15a
Guto Xavier <gutoffline@gmail.com>
2022-05-21 13:52:00 -0300

    trabalhando com funções

--- aula-06/atividade02.py
+++ aula-06/atividade02.py
@@ -1,4 +1,7 @@
-def soma(n1,n2):
+from os import system
+from datetime import date
+
+def somar(n1,n2):
     total = float(n1) + float(n2)
     print(total)
 
@@ -14,9 +17,29 @@ def multiplicar(n1, n2):
     total = float(n1) * float(n2)
     print(total)
 
+def menu():
+    system("cls")
+    print("ESCOLHA A OPERAÇÃO QUE SERÁ EXECUTADA:")
+    print("1 - SOMA")
+    print("2 - SUBTRAÇÃO")
+    print("3 - MULTIPLICAÇÃO")
+    print("4 - DIVISÃO")
+    opcao = int(input("INFORME A OPÇÃO: "))
+    return opcao
+    
+system("cls")
+print(date.today())
 num1 = input("Informe um número: ")
 num2 = input("Informe outro número: ")
-soma(num1,num2)
-subtrair(num1,num2)
-dividir(num1, num2)
-multiplicar(num1, num2)
+
+operacaoEscolhida = menu()
+if operacaoEscolhida == 1:
+    somar(num1, num2)
+elif operacaoEscolhida == 2:
+    subtrair(num1, num2)
+elif operacaoEscolhida == 3:
+    multiplicar(num1 , num2)
+elif operacaoEscolhida == 4:
+    dividir(num1, num2)
+else:
+    print("Operação inválida")
