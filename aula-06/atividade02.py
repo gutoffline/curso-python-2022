8560f894f45e7cbf2da94c3e5025c0caa6983bc2
Guto Xavier <gutoffline@gmail.com>
2022-05-21 13:14:15 -0300

    trabalhando com funções

---
+++ aula-06/atividade02.py
@@ -0,0 +1,22 @@
+def soma(n1,n2):
+    total = float(n1) + float(n2)
+    print(total)
+
+def subtrair(n1, n2):
+    total = float(n1) - float(n2)
+    print(total)
+
+def dividir(n1, n2):
+    total = float(n1) / float(n2)
+    print(total)
+
+def multiplicar(n1, n2):
+    total = float(n1) * float(n2)
+    print(total)
+
+num1 = input("Informe um número: ")
+num2 = input("Informe outro número: ")
+soma(num1,num2)
+subtrair(num1,num2)
+dividir(num1, num2)
+multiplicar(num1, num2)
