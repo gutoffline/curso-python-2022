c74938ea944032dbd90b77887de07700d5c69c14
Guto Xavier <gutoffline@gmail.com>
2022-05-21 12:12:12 -0300

    Atividade 01 - Funções

---
+++ aula-06/atividade01.py
@@ -0,0 +1,27 @@
+# FUNÇÕES
+# conjunto de código agrupado e nomeado. Sempre que chamar o nome da função, ela será executada.
+"""
+Ex 01:
+def nome-da-funcao():
+    bloco de código
+    bloco de código
+    bloco de código
+    bloco de código
+"""
+# O nome da função segue as mesmas regras de nome de variável(sem espaço, sem caracteres especiais, lembrar que diferencia maiúsculas de minúsculas). A recomendação é sempre um nome significativo.
+# Uma função, pode ou não receber parâmetros. No Ex01 a função não recebe parâmetro. No Ex02 a função espera receber parâmetros.
+"""
+Ex 02:
+def nome-da-funcao(parametro1, parametro2):
+    bloco de código
+    bloco de código
+    bloco de código
+    bloco de código
+"""
+
+def mensagem(texto):
+    print(texto)
+
+mensagem("Trabalhando com funções")
+mensagem("Atividade 01")
+mensagem("21/5/2022")
