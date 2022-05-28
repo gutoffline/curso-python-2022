06adb3af1fff4c0b2fb9282a9c5880ac95bd74d1
Guto Xavier <gutoffline@gmail.com>
2022-05-28 10:52:45 -0300

    operacoes matematicas

---
+++ aula-07/operacoes.py
@@ -0,0 +1,21 @@
+# salvar como operacoes.py
+def soma(n1,n2):
+    resultado = n1 + n2
+    print(resultado)
+    
+def subtrair(n1,n2):
+    resultado = n1 - n2
+    print(resultado)
+
+def dividir(n1,n2):
+    resultado = n1/n2
+    print(resultado)
+    
+def multiplicar(n1,n2):
+    resultado = n1*n2
+    print(resultado)
+    
+soma(10,5)
+subtrair(10,5)
+dividir(10,5)
+multiplicar(10,5)
